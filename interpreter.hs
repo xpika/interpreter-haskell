cdbd177a686a09c10d482cdcf51fdd368eaa78f8
alan hawkins <hawk.alan@gmail.com>
2013-12-19 23:30:33 +1100

    extra files

---
+++ interpreter.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Web.Scotty
+import Control.Monad.Trans
+import Data.Text.Lazy
+import Language.Haskell.Interpreter hiding (get)
+
+import Data.Monoid (mconcat)
+
+main = scotty 3000 $ do
+  get "/ghci_command/:word" $ do
+    beam <- param "word"
+    interpreted <- liftIO $  runInterpreter $ setImports ["Prelude","Control.Monad"] >> eval beam
+    html $ mconcat [pack (show interpreted)]
+  get "/:word" $ do
+    beam <- param "word"
+    newfile <- liftIO $ readFile "text.html"
+    html $ mconcat [pack newfile,beam]
