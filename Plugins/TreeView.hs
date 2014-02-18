38eb32f3051df80cd8677d453a5b88b77d2294b1
alan hawkins <hawk.alan@gmail.com>
2014-02-18 19:26:26 +1100

    added tree viewing function2

---
+++ Plugins/TreeView.hs
@@ -0,0 +1,26 @@
+module Plugins.TreeView where
+
+import Data.Tree
+import Plugins.LiteralString
+
+ -- wip
+
+data HTMLTree = HTMLTree (Tree String)
+
+testTree = Node "" [
+              Node "c1" [ Node "1" [], Node "2" [] ]
+             ,Node "c2" [ Node "1" [], Node "2" [] ]
+           ]
+
+renderList xs = 
+    "<ul>" 
+  ++ concatMap (\item -> "<li>" ++ show (str item) ++ "</li>") xs
+  ++ "</ul>"
+
+instance Show (HTMLTree) where
+  show (HTMLTree s) = renderList . map ((\x -> "<a style=\"text-decoration:none\" href=\"#\">[+]</a> "++ x) . rootLabel)  $  subForest s
+ 
+
+
+renderTree = HTMLTree testTree
+
