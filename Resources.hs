e5f6222baff10d59f157faba2034fd0c5b210fa9
Alan Hawkins <hawk.alan@gmail.com>
2014-02-24 16:42:33 +1100

    disabled optional plugins

--- Resources.hs
+++ Resources.hs
@@ -1,13 +1,12 @@
 module Resources (
-  module Data.List
+ -- module Data.List
  ,module Plugins.LiteralString
- ,module Plugins.DiagramStuff
+ --,module Plugins.DiagramStuff
 ) where
 
-import Plugins.DiagramStuff
-import Data.List
+-- import Plugins.DiagramStuff
+-- import Data.List
 import Plugins.LiteralString
-import Plugins.Charts
-import Plugins.TreeView
-import Plugins.Html as H
-
+-- import Plugins.Charts
+-- import Plugins.TreeView
+-- import Plugins.Html as H
